12dbbbdc290089d12c787949c32dd47055384624
wirenpaper <chumchumchira@gmail.com>
2026-03-21 07:44:17 +0500

    fix emacs redo setting

--- init.el
+++ init.el
@@ -58,7 +58,8 @@
   :init
   (setq evil-want-integration t
 	evil-want-keybinding nil
-	evil-want-C-u-scroll t)
+	evil-want-C-u-scroll t
+    	evil-undo-system 'undo-redo)
   :config
   (evil-mode 1)
   (evil-set-leader 'normal (kbd "SPC"))
